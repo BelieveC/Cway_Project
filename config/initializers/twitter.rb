08110d28df77ff000ce2d00d435dd3558a1de5fb
Chaitanya Yadav <cayadav2009@gmail.com>
2016-04-19 01:46:15 +0530

    Tweet

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-client = Twitter::REST::Client.new do |config|
+$client = Twitter::REST::Client.new do |config|
   config.consumer_key        = "2FIxgSgaAZqDI2tbL6SZUmGko"
   config.consumer_secret     = "12EeTBNAkbp0T4du7b1ptYBuM6OO3o37lUU5kY0ALwgIRmu1YN"
   config.access_token        = "247751934-wflbOhoP2Awrm8yoqyP3d4M3IHTCLfUXKyTgpz14"
